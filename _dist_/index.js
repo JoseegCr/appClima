02b01cfd100d8d907c50645ff69e03b99e7f8af6
JoseegCr <JoseegCr@users.noreply.github.com>
2022-03-01 00:42:27 +0000

    deploy: 2718c2d14c77391dd2d82a2d80251878ad435d5c

--- _dist_/index.js
+++ _dist_/index.js
@@ -1,11 +1,36 @@
 import axios from '../_snowpack/pkg/axios.js';
 
+
+const image = document.querySelector('img');
 const clima = document.querySelector('#clima');
 const ciudad = document.querySelector('#ciudad');
 const temperatura = document.querySelector('#temperatura');
-const form = document.querySelector('#form');
 const search = document.querySelector('#search');
-const img = document.querySelector('#img');
+const form =  document.querySelector('form');
 
-const apiKey = '';
-const iconUrl = '';
+const apiKey = 'd23a1ce2f2961252cbdab5be4a975343';
+const iconUrl = 'http://openweathermap.org/img/wn/';
+
+const getData = async ()=>{
+    const res = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=Apizaco&appid=${apiKey}&units=metric`);
+    const info = res.data.weather[0];
+
+    const icon = info.icon;
+    const climaTemp = info.main;
+    const cityName = res.data.name;
+    const temp = res.data.main.temp;
+
+    image.src=`${iconUrl}/${icon}@2x.png`;
+    clima.textContent = climaTemp;
+    ciudad.textContent = cityName;
+    temperatura.textContent = `${temp}°C`;
+}
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    const city = search.value;
+    console.log(city);
+});
+
+getData();
